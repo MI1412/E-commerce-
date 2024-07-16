1ef6ca278e2aecf498aa6294d1294ad2fc3df635
MI1412 <imronm1309@gmail.com>
2024-07-16 18:03:22 +0700

    start project 16-07-2024

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>halaman about</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ab illo consectetur officia fugit exercitationem accusamus adipisci debitis itaque eveniet, unde consequatur ut quibusdam ipsa rem? Vel, dicta! Dicta, labore.</p>
+    <a href="/">home</a>
+</body>
+</html>
